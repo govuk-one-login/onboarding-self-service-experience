d3292450b7a279e759f9afc96a0b3d5c2537e31c
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-05-16 11:03:34 +0100

    Add Service dashboard update view and move testing routes from main app file

---
+++ express/src/routes/testing-routes.ts
@@ -0,0 +1,35 @@
+import express, {Request, Response} from 'express';
+
+const router = express.Router();
+
+//Testing route for service name get request
+router.get('/add-service-name', (req, res) => {
+  res.render("add-service-name.njk");
+});
+
+// Testing route for service name  get request with error
+router.get('/add-service-name-error', (req, res) => {
+  let errorMessages: Map<String, String> = new Map<String, String>();
+  errorMessages.set('serviceName', 'Enter your service name');
+  res.render("add-service-name.njk",{ errors: errorMessages });
+});
+
+// Testing route for client details dashboard
+router.get('/service-dashboard-client-details', (req, res) => {
+  res.render("service-dashboard-client-details.njk");
+});
+
+// Testing route for client details dashboard update view
+router.get('/service-dashboard-client-details-update', (req, res) => {
+  res.render("service-dashboard-client-details-update.njk");
+});
+
+// Testing route for client details dashboard update view with errors
+router.get('/service-dashboard-client-details-update-error', (req, res) => {
+  let errorMessages: Map<String, String> = new Map<String, String>();
+  errorMessages.set('clientNameDetails', 'Enter a client name');
+  errorMessages.set('redirectUrl', 'Enter a redirect URI');
+  res.render("service-dashboard-client-details-update.njk",{ errors: errorMessages });
+});
+
+export default router;
