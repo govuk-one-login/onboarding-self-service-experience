c8b3bfdd7f39437959c65dfd8fb4cefb4ea9b736
tinokingstone <tino.kingstone@digital.cabinet-office.gov.uk>
2022-05-18 14:36:55 +0100

    phone number is validated

--- express/src/middleware/emailValidator.ts
+++ express/src/middleware/emailValidator.ts
@@ -1,26 +1,44 @@
 import {NextFunction, Request, Response} from "express";
+import allowedEmailDomains from "../lib/allowedEmailDomains";
 
-export function emailValidator(req: Request, res: Response, next: NextFunction) {
+import isRfc822Compliant from "../lib/isRfc822Compliant";
+
+export async function emailValidator(req: Request, res: Response, next: NextFunction) {
     let emailAddress: string = req.body.emailAddress;
-    // trim
-    // ensure well-formed
-    // correct suffix
 
-    // modify req.body.email
+    emailAddress = emailAddress.trim();
 
-    if(true) {
-
-        req.body.emailAddress = emailAddress;
-        next();
-    } else {
-        const errorMessages = new Map<string, string>();
-        const values = new Map<string, string>();
-        errorMessages.set("emailAddress", "You must provide a government email address");
-        values.set('emailAddress', req.body.emailAddress);
-        res.render('create-account/get-email.njk', {
-            errorMessages: errorMessages,
-            values: values,
-            fieldOrder: ['emailAddress']
-        });
+    if (emailAddress === "" || emailAddress === undefined || emailAddress === null) {
+        await errorResponse(emailAddress, res, 'emailAddress', 'Please ensure that all fields have been filled in correctly.');
+        return;
     }
+
+    if (!isRfc822Compliant(emailAddress)) {
+        await errorResponse(emailAddress, res, 'emailAddress', 'Please check that your email is formatted correctly.');
+        return;
+    }
+
+    if (!await isAllowedDomain(emailAddress)) {
+        await errorResponse(emailAddress, res, 'emailAddress', 'Please ensure that you are using a .gov.uk email address.');
+        return;
+    }
+
+    req.session.emailAddress = emailAddress;
+    next();
+}
+
+export function errorResponse(emailAddress: string, res: Response, key: string, message: string) {
+    const errorMessages = new Map<string, string>();
+    const values = new Map<string, string>();
+    errorMessages.set(key, message);
+    values.set('emailAddress', emailAddress);
+    res.render('create-account/get-email.njk', {
+        errorMessages: errorMessages,
+        values: values,
+        fieldOrder: ['emailAddress']
+    });
+}
+
+export async function isAllowedDomain(emailAddress: string): Promise<boolean> {
+    return (await allowedEmailDomains).filter((domain: string) => emailAddress.endsWith(`.${domain}`)).length > 0;
 }
