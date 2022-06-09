d549d4c8d7a0065f608e7bbc2f8f25f24ff42725
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-06-09 12:10:43 +0100

    Update unit tests for password validator

--- express/src/middleware/passwordValidator.spec.ts
+++ express/src/middleware/passwordValidator.spec.ts
@@ -3,16 +3,20 @@ import {eightDigitsMinimum} from "./passwordValidator";
 
 const PASSWORD_WITH_EMPTY_VALUE = '';
 const PASSWORD_LESS_THAN_8_CHAR = '123';
-const PASSWORD_8_CHAR_MINIMUM = '12345678';
+const PASSWORD_8_CHAR = '12345678';
+const PASSWORD_9_CHAR = '123456789';
 
 describe('Checking that the user has entered a valid password', () => {
-    it('a valid +4479 number is accepted. ', async function () {
-        assert.equal(eightDigitsMinimum(PASSWORD_8_CHAR_MINIMUM), true, `Expected ${PASSWORD_8_CHAR_MINIMUM} to be allowed because it has 8 or more characters`);
+    it('a valid "12345678" 8 character password is accepted. ', async function () {
+        assert.equal(eightDigitsMinimum(PASSWORD_8_CHAR), true, `Expected ${PASSWORD_8_CHAR} to be allowed because it has 8 or more characters`);
     });
-    it('a valid +4479 number is accepted. ', async function () {
+    it('a valid "123456789" more than 8 character password is accepted. ', async function () {
+        assert.equal(eightDigitsMinimum(PASSWORD_9_CHAR), true, `Expected ${PASSWORD_9_CHAR} to be allowed because it has 8 or more characters`);
+    });
+    it('a password with less than 8 characters is not accepted. ', async function () {
         assert.equal(eightDigitsMinimum(PASSWORD_LESS_THAN_8_CHAR), false, `Expected ${PASSWORD_LESS_THAN_8_CHAR} not to be allowed because it does not have 8 or more characters`);
     });
-    it('a valid +4479 number is accepted. ', async function () {
+    it('a password with empty value is not accepted. ', async function () {
         assert.equal(eightDigitsMinimum(PASSWORD_WITH_EMPTY_VALUE), false, `Expected ${PASSWORD_WITH_EMPTY_VALUE} not to be allowed because it has empty value`);
     });
 });
