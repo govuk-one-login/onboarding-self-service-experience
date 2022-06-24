3df2bdf977a160d41597537dda730bf1750e540a
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-06-24 10:54:18 +0100

    Update with latest main changes, add links to change button on dashboard, remove unnecessary files and their refferences

--- express/src/routes/testing-routes.ts
+++ express/src/routes/testing-routes.ts
@@ -2,43 +2,6 @@ import express, {Request, Response} from 'express';
 
 const router = express.Router();
 
-//Testing route for service name get request
-
-// Testing route for service name  get request with error
-router.get('/add-service-name-error', (req, res) => {
-  let errorMessages: Map<String, String> = new Map<String, String>();
-  errorMessages.set('serviceName', 'Enter your service name');
-  res.render("add-service-name.njk",{ errorMessages: errorMessages });
-});
-
-// Testing route for client details dashboard
-router.get('/client-details', (req, res) => {
-  res.render("service-dashboard-client-details.njk", {
-      publicKeyAndUrlsNotUpdatedByUser: true,
-      userDetailsUpdated: false,
-      clientName: "My juggling service",
-      serviceName: "My juggling service",
-      clientId: "X8rnOEw0SqOdEAOnWyPQ6YsJugQ",
-      redirectUrls: "https://get-a-juggling-licence.gov.uk/redirect/endpoint",
-      userAttributesRequired: "Email address<br>Phone number",
-      userPublicKey: "Not yet added",
-      postLogoutRedirectUrls: "Not yet added"
-  });
-});
-
-// Testing route for client details dashboard update view
-router.get('/service-dashboard-client-details-update', (req, res) => {
-  res.render("service-dashboard-client-details-update.njk");
-});
-
-// Testing route for client details dashboard update view with errors
-router.get('/service-dashboard-client-details-update-error', (req, res) => {
-  let errorMessages: Map<String, String> = new Map<String, String>();
-  errorMessages.set('clientNameDetails', 'Enter a client name');
-  errorMessages.set('redirectUrl', 'Enter a redirect URI');
-  res.render("service-dashboard-client-details-update.njk",{ errors: errorMessages });
-});
-
 // Testing routes for Change your client name page
 router.get('/change-client-name', (req, res) => {
     res.render("dashboard/change-client-name.njk", {
@@ -54,8 +17,7 @@ router.post('/change-client-name', (req, res) => {
         res.render('dashboard/change-client-name.njk', {errorMessages: errorMessages});
         return;
     }
-    res.redirect('/service-dashboard-client-details');
+    res.redirect('/client-details');
 });
-
 
 export default router;
