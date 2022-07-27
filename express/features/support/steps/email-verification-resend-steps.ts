646b9b32ee63b9fb35a604774f2bde4c75296b75
Karol C. Gancarz <karol.sinx@gmail.com>
2022-07-27 12:40:47 +0100

    BAU: Apply formatting to feature tests
    Apply formatting
    Optimise imports
    Use one kind of quotes for strings (single quotes)

--- express/features/support/steps/email-verification-resend-steps.ts
+++ express/features/support/steps/email-verification-resend-steps.ts
@@ -1,12 +1,9 @@
-
-   
-import {Given} from "@cucumber/cucumber";
+import { Given } from '@cucumber/cucumber';
 
 Given('that the user is on the `Check your email` page', async function () {
-  await this.goToPath('/create/get-email');
-  await this.page.type("#emailAddress", 'registering-successfully@gds.gov.uk');
-  await this.page.waitForSelector('#submit');
-  await this.page.click('#submit');
-  await this.page.waitForSelector('#resend-code-page');       
+    await this.goToPath('/create/get-email');
+    await this.page.type('#emailAddress', 'registering-successfully@gds.gov.uk');
+    await this.page.waitForSelector('#submit');
+    await this.page.click('#submit');
+    await this.page.waitForSelector('#resend-code-page');
 });
-
--- express/features/support/steps/shared-steps.ts
+++ express/features/support/steps/shared-steps.ts
@@ -1,6 +1,7 @@
-import {Given, When, Then} from "@cucumber/cucumber";
-import {strict as assert} from "assert";
-import {getLink, checkUrl} from './shared-functions';
+import { Given, Then, When } from '@cucumber/cucumber';
+import { strict as assert } from 'assert';
+import { Page } from "puppeteer";
+import { checkUrl, getLink } from './shared-functions';
 
 Given('that the user is on the {string} page', async function (route: string) {
     await this.goToPath(route);
@@ -9,7 +10,7 @@ Given('that the user is on the {string} page', async function (route: string) {
 When('they click on the {string} link', async function (text: string) {
     let links = await this.page.$x(`//a[contains(., '${text}')]`);
     await Promise.all([
-        this.page.waitForNavigation({timeout: 10000}),
+        this.page.waitForNavigation({ timeout: 10000 }),
         links[0].click()
     ]);
 });
@@ -17,7 +18,7 @@ When('they click on the {string} link', async function (text: string) {
 When('they click on the {string} button-link', async function (text: string) {
     let links = await this.page.$x(`//a[contains(., '${text}') and contains(concat(" ", normalize-space(@class), " "), " govuk-button ")]`);
     await Promise.all([
-        this.page.waitForNavigation({timeout: 10000}),
+        this.page.waitForNavigation({ timeout: 10000 }),
         links[0].click()
     ]);
 });
@@ -25,7 +26,7 @@ When('they click on the {string} button-link', async function (text: string) {
 When('they select the Submit button', async function () {
     await Promise.all([
         this.page.waitForNavigation(),
-        this.page.click("#submit")
+        this.page.click('#submit')
     ]);
 });
 
@@ -59,7 +60,7 @@ Then('the error message {string} must be displayed for the {string} radios', asy
 
 Then('they should see the text {string}', async function (text) {
     let bodyText: string = await this.page.$eval('body', (element: any) => element.textContent)
-    assert.equal(bodyText.includes(text), true, `Body text does not contain "${text}"`)
+    assert.equal(bodyText.includes(text), true, `Body text does not contain '${text}'`)
 });
 
 Then('the {string} link will point to the following URL: {string}', async function (linkText, expectedUrl) {
