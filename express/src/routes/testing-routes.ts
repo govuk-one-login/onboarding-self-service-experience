9a011f2d1d9dc8301642e42fa2d6b4447368555b
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-07-28 14:44:20 +0100

    DFA-2029 Create the 'forgot your password' page

--- express/src/routes/testing-routes.ts
+++ express/src/routes/testing-routes.ts
@@ -450,4 +450,11 @@ router.get('/there-is-a-problem-test', (req, res) => {
     res.render("there-is-a-problem.njk");
 });
 
+// Testing routs Create the 'forgot your password' page
+router.get('/forgot-password', (req, res) => {
+    res.render("forgot-password.njk", {
+        emailAddress: 'your.email@digital.cabinet-office.gov.uk'
+    });
+});
+
 export default router;
