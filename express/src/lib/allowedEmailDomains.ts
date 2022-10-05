c381b799da7326e39ebe2e50bda71b45e1f9bdef
Karol C. Gancarz <karol.sinx@gmail.com>
2022-10-05 11:44:51 +0100

    DFA-2048: Organise resource files

--- express/src/lib/allowedEmailDomains.ts
+++ express/src/lib/allowedEmailDomains.ts
@@ -1,7 +1,7 @@
 import fs from "fs/promises";
 
 async function loadEmails(): Promise<string[]> {
-    const data = fs.readFile("./valid-email-domains.txt", {
+    const data = fs.readFile("resources/allowed-email-domains.txt", {
         encoding: "utf8"
     });
     return (await data).split("\n");
