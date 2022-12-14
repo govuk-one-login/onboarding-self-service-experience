040e2eab7ffdfa96761f932254c24d5c35e21bda
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-12-14 22:54:33 +0000

    DFA-2342 - Accessibility update for show/hide public key script and view for Client details

--- express/assets/javascripts/toggle-public-key-view.js
+++ express/assets/javascripts/toggle-public-key-view.js
@@ -14,11 +14,17 @@ function showFullKey() {
         buttonText.innerHTML = "Show the full public key";
         publicKeyShort.style.display = "block";
         publicKeyLong.style.display = "none";
+        togglePublicKey.setAttribute("aria-expanded", "false");
+        publicKeyLong.removeAttribute("aria-labelledby");
+        publicKeyShort.setAttribute("aria-labelledby", "public-key");
     } else {
         publicKeyContainer.className = "open";
         toggleTextChevron.className = "govuk-accordion-nav__chevron";
         buttonText.innerHTML = "Hide the full public key";
         publicKeyShort.style.display = "none";
         publicKeyLong.style.display = "block";
+        togglePublicKey.setAttribute("aria-expanded", "true");
+        publicKeyShort.removeAttribute("aria-labelledby");
+        publicKeyLong.setAttribute("aria-labelledby", "public-key");
     }
 }
