1684008bf9771546a38a97d0d5fb2c386d1046b4
John Watts <john.watts@digital.cabinet-office.gov.uk>
2022-12-29 17:36:00 +0000

    SSE-2385 public key data
    Add feature tests.
    
    Trim space from public key middleware so it works for certs without headers that have trailing (or leading) space.

--- express/src/services/lambda-facade/StubLambdaFacade.ts
+++ express/src/services/lambda-facade/StubLambdaFacade.ts
@@ -22,8 +22,9 @@ class StubLambdaFacade implements LambdaFacadeInterface {
         phone: {S: "07700 987 654"}
     };
 
-    publicKey =
+    private DEFAULT_PUBLIC_KEY =
         "MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAp2mLkQGo24Kz1rut0oZlviMkGomlQCH+iT1pFvegZFXq39NPjRWyatmXp/XIUPqCq9Kk8/+tq4Sgjw+EM5tATJ06j5r+35of58ATGVPniW//IhGizrv6/ebGcGEUJ0Y/ZmlCHYPV+lbewpttQ/IYKM1nr3k/Rl6qepbVYe+MpGubluQvdhgUYel9OzxiOvUk7XI0axPquiXzoEgmNNOai8+WhYTkBqE3/OucAv+XwXdnx4XHmKzMwTv93dYMpUmvTxWcSeEJ/4/SrbiK4PyHWVKU2BozfSUejVNhahAzZeyyDwhYJmhBaZi/3eOOlqGXj9UdkOXbl3vcwBH8wD30O9/4F5ERLKxzOaMnKZ+RpnygWF0qFhf+UeFMy+O06sdgiaFnXaSCsIy/SohspkKiLjNnhvrDNmPLMQbQKQlJdcp6zUzI7Gzys7luEmOxyMpA32lDBQcjL7KNwM15s4ytfrJ46XEPZUXESce2gj6NazcPPsrTa/Q2+oLS9GWupGh7AgMBAAE=";
+    publicKey = this.DEFAULT_PUBLIC_KEY;
 
     serviceName = "";
 
