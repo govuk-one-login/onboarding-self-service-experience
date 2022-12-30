92d1c4e90e68f300420b6ba0ffa5105f7de1f425
Borislav Petrov <boris.petrov@digital.cabinet-office.gov.uk>
2022-12-30 15:22:49 +0000

    SSE-2384 - Remove the 'back' link on the 'Resend security code' page

--- express/src/routes/manage-account.ts
+++ express/src/routes/manage-account.ts
@@ -83,7 +83,6 @@ router.post("/change-service-name/:serviceId/:selfServiceClientId/:clientId", ch
 
 router.get("/account/resend-phone-code", checkAuthorisation, (req, res) => {
     res.render("common/resend-security-code.njk", {
-        backLinkPath: "/account/verify-phone-code",
         securityCodeMethod: "phone"
     });
 });
