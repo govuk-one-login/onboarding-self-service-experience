52b6fed93092961972afca462da66f2ab161b683
John Watts <john.watts@digital.cabinet-office.gov.uk>
2023-01-06 12:24:49 +0000

    BAU Fix public key utils
    Do global find and replace on returned public key.
    
    Change logging to only log at the end.
    
    Co-authored by: Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>

--- express/src/lib/publicKeyUtils.ts
+++ express/src/lib/publicKeyUtils.ts
@@ -7,18 +7,16 @@ export default function getAuthApiCompliantPublicKey(publicKey: string): string 
     // assume PEM with headers
     try {
         createPublicKey({key: publicKey, format: "pem"});
-        return publicKey.replace(BEGIN, "").replace(END, "").replace("\n", "").replace("\r", "");
-    } catch (err) {
-        console.error(`Failed to convert\n${publicKey}`);
-    }
-
+        return publicKey.replace(BEGIN, "").replace(END, "").replace(/\n/g, "").replace(/\r/g, "");
+    } catch (ignored) {}
+    // assume they didn't supply headers
     const publicKeyWithHeaders = `${BEGIN}\n${publicKey}\n${END}\n`;
+
     try {
         createPublicKey({key: publicKeyWithHeaders, format: "pem"});
-        return publicKeyWithHeaders.replace(BEGIN, "").replace(END, "").replace("\n", "").replace("\r", "");
+        return publicKeyWithHeaders.replace(BEGIN, "").replace(END, "").replace(/\n/g, "").replace(/\r/g, "");
     } catch (err) {
-        console.error(`Failed to convert\n${publicKeyWithHeaders}`);
+        console.error(err);
+        throw new Error(`Failed to convert\n${publicKey}\n`);
     }
-
-    throw new Error(`Failed to convert\n${publicKey}\n`);
 }
