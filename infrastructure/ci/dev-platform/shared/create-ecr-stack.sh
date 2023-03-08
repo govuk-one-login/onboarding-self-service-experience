bd500a3b54bdfddb946def4602e61f1bc547bd71
abdul.aziz <abdul.aziz@digital.cabinet-office.gov.uk>
2023-03-08 15:37:31 +0000

    fixed shellcheck code quality errors

--- infrastructure/ci/dev-platform/shared/create-ecr-stack.sh
+++ infrastructure/ci/dev-platform/shared/create-ecr-stack.sh
@@ -20,7 +20,7 @@ aws cloudformation create-stack --stack-name container-image-repository \
   --template-url https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/container-image-repository/template.yaml \
   --region eu-west-2 \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-  --parameters ParameterKey=AWSOrganizationId,ParameterValue='o-pjzf8d99ys\,o-dpp53lco28' \
+  --parameters ParameterKey=AWSOrganizationId,ParameterValue='o-pjzf8d99ys\,o-dpp53lco28' ParameterKey=PipelineStackName,ParameterValue="$PIPELINE_STACK_NAME" \
   --tags Key=Product,Value="GOV.UK Sign In" \
   Key=System,Value="Onboarding Self-Service" \
   Key=Environment,Value="$ACCOUNT" \
