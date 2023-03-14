0c91ab8f5703f62053da708940ba43d1afdc826c
Karol C. Gancarz <karol.sinx@gmail.com>
2023-03-14 21:25:56 +0000

    SSE-2521: Import ECS roles from the frontend stack into the GHA role
    Referring to the roles by the stack prefix is not reliable and has recently stopped working. Use a more robust and secure method by using SSM parameters to reference ECS task roles.
    
    Automatically write the ECS task role ARNs to SSM parameters to be used at deploy time.

--- infrastructure/frontend/apply-frontend-infrastructure.sh
+++ infrastructure/frontend/apply-frontend-infrastructure.sh
@@ -4,6 +4,7 @@ set -eu
 BASE_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 "$BASE_DIR"/../deploy-sam-stack.sh "$@" \
+  --validate \
   --account development \
   --stack-name self-service-frontend-infrastructure \
   --template "$BASE_DIR"/sse-frontend-infrastructure.yml \
