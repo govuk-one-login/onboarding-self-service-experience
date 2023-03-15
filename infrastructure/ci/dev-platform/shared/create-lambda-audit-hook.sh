ca2b0eb38096eac4a1d118d448c7fa4233952bdc
John Watts <john.watts@digital.cabinet-office.gov.uk>
2023-03-15 17:35:21 +0000

    SSE-2511 Lambda and Infrastructure hooks in build account
    Write shared scripts to create lamdba and infrastructure provencence check hooks.
    
    Write scripts to invoke shared scripts for build account.

---
+++ infrastructure/ci/dev-platform/shared/create-lambda-audit-hook.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -eu
+
+if [[ "$#" -ne 1 || ! "development  build  staging  integration production" =~ ( |^)$1( |$) ]]; then
+  echo "Usage: $0 <development | build | staging | integration | production>"
+  exit 1
+fi
+
+BASE_DIR="$(dirname "${BASH_SOURCE[0]}")"
+pushd "$BASE_DIR" > /dev/null
+
+ACCOUNT="$1"
+
+../../../check-aws-account.sh "$ACCOUNT" || exit
+
+if [[ "$ACCOUNT" == "development" ]]; then
+  DEVPLATFORM_ENVIRONMENT="dev"
+else
+  DEVPLATFORM_ENVIRONMENT="$ACCOUNT"
+fi
+
+aws cloudformation create-stack --stack-name lambda-audit-hook \
+  --template-url https://template-storage-templatebucket-1upzyw6v9cs42.s3.amazonaws.com/lambda-audit-hook/template.yaml \
+  --region eu-west-2 \
+  --capabilities CAPABILITY_IAM \
+  --tags Key=Product,Value="GOV.UK Sign In" \
+         Key=System,Value="Onboarding Self-Service" \
+         Key=Environment,Value="$DEVPLATFORM_ENVIRONMENT" \
+         Key=Owner,Value="DI self-service team"
