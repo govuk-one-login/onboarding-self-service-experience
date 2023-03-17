622e1e80814fdc7770f06444b7a10faa61b4352d
Karol C. Gancarz <karol.sinx@gmail.com>
2023-03-17 18:43:07 +0000

    SSE-2496: Add the required AWS permissions for the GHA role to deploy API lambdas
    The permissions allow to create, delete, tag and untag the required resources.
    
      - Split the GitHub actions role permissions into separate policies to address a Checkov error
      - Expand the GitHub Actions template to include the notification email param
      - Move the OIDC provider to the stack from this repo
    
    Crude temporary fix for frontend role permissions:
    Relying on resource names is not reliable since they're randomly generated and may get trimmed.
    This will be fixed properly in SSE-2521. Use a modified wildcard pattern for now.

--- infrastructure/ci/apply-preview-stacks-config.sh
+++ infrastructure/ci/apply-preview-stacks-config.sh
@@ -2,26 +2,10 @@
 set -eu
 
 BASE_DIR="$(dirname "${BASH_SOURCE[0]}")"
-pushd "$BASE_DIR" > /dev/null
-../check-aws-account.sh development > /dev/null
 
-PROVIDER_ARN=$(aws iam list-open-id-connect-providers \
-  --query "OpenIDConnectProviderList[?contains(Arn, 'token.actions.githubusercontent.com')].Arn" \
-  --output text)
-
-FRONTEND_STACK=$(aws cloudformation describe-stacks \
-  --stack-name sse-frontend-infrastructure \
-  --query "Stacks[].StackName" \
-  --output text 2> /dev/null)
-
-../deploy-sam-stack.sh "$@" \
+"$BASE_DIR"/../deploy-sam-stack.sh "$@" \
+  --validate \
   --account development \
   --stack-name github-actions-preview-stacks-config \
-  --template github-actions-preview-stacks.yml \
-  --tags sse:stack-type=config sse:stack-role=deployment \
-  --params \
-  OIDCProviderArn="$PROVIDER_ARN" \
-  CreateDeploymentArtifactsBucket=true \
-  FrontendInfrastructureStack="${FRONTEND_STACK:-}"
-
-popd > /dev/null
+  --template "$BASE_DIR"/github-actions-preview-stacks.yml \
+  --tags sse:stack-type=config sse:stack-role=deployment
