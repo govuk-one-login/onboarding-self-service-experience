f45bbdb1515c5dcf2f5de12cbdc345d1f3c2e413
Karol C. Gancarz <karol.sinx@gmail.com>
2023-05-23 09:56:19 +0100

    SSE-2590: Use nested stacks to deploy pipelines
    - Add a nested stack for deployment pipelines
    - Add a script to deploy the pipeline stacks to any account
    - Pull account config from the accounts json file
    - Use exports for notifications and VPC
    - Correctly set downstream accounts
      For an initial account with no downstream accounts, set downstream accounts to itself
    
    - Enable promotion
      Retrieve bucket ARNs from the previous account
    
    - Add VPC to the frontend and lambdas stacks

--- infrastructure/ci/dev-platform/build/deploy-deployment-pipeline-stack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-BASE_DIR="$(dirname "${BASH_SOURCE[0]}")"
-pushd "$BASE_DIR" > /dev/null
-../shared/deploy-deployment-pipeline-stack.sh build "${1:-}" "${2:-}"
-popd > /dev/null
