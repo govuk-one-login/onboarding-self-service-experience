25c01364f52469cc191bce11b2629e4eef37f359
Karol C. Gancarz <karol.sinx@gmail.com>
2023-06-13 19:23:27 +0100

    SSE-2685: Deploy backend components to secure pipelines
    Modify the API workflow to account for the filepath change
    Add jobs to build and upload the API to secure pipelines
    Upload the secure pipelines workflow to use reusable workflows for each component
    
    Add the Auth API key to the deployment parameters script
    Add methods to handle secrets
    Add an array with secret names that need to be set in the account
    Make the script methods more reusable

--- infrastructure/ci/configure-deployment-parameters.sh
+++ infrastructure/ci/configure-deployment-parameters.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
-# Set up Parameter Store parameters required to deploy Admin Tool stacks
+# Set up Parameter Store parameters and Secrets Manager secrets required to deploy Admin Tool stacks
 cd "$(dirname "${BASH_SOURCE[0]}")"
 set -eu
 
 ACCOUNT=$(../aws.sh get-current-account-name)
 PARAMETER_NAME_PREFIX=/self-service
+MANUAL_PARAMETERS=(api_notification_email)
 
 declare -A PARAMETERS=(
   [cognito_external_id]=$PARAMETER_NAME_PREFIX/cognito/external-id
   [deletion_protection]=$PARAMETER_NAME_PREFIX/config/deletion-protection-enabled
+  [api_notification_email]=$PARAMETER_NAME_PREFIX/api/notification-email
+)
+
+declare -A SECRETS=(
+  [auth_api_key]=$PARAMETER_NAME_PREFIX/api/auth-api-key
+  [notify_api_key]=$PARAMETER_NAME_PREFIX/cognito/notify-api-key
 )
 
 function check-parameter-set {
   [[ $(xargs < <(get-parameter-value "$1")) ]]
+}
+
+function check-secret-set {
+  aws secretsmanager describe-secret --secret-id "$1" &> /dev/null
 }
 
 function get-parameter-value {
@@ -21,7 +32,35 @@ function get-parameter-value {
 
 function write-parameter-value {
   echo "Setting '$1' to '$2'"
-  aws ssm put-parameter --name "${PARAMETERS[$1]}" --value "$2" --type String --overwrite > /dev/null
+  aws ssm put-parameter --name "${PARAMETERS[$1]}" --value "$(xargs <<< "$2")" --type String --overwrite > /dev/null
+}
+
+function write-secret-value {
+  echo "Setting secret '$1'"
+  aws secretsmanager create-secret --name "$1" --secret-string "$(xargs <<< "$2")" > /dev/null
+}
+
+function get-value-from-user {
+  local name=$1 type=${2:-parameter} value
+  while [[ -z $(xargs <<< "${value:-}") ]]; do read -rp "Enter a value for the $type '$name': " value; done
+  echo "$value"
+}
+
+function check-manual-parameters {
+  local parameter
+  for parameter in "${MANUAL_PARAMETERS[@]}"; do
+    check-parameter-set "$parameter" || write-parameter-value "$parameter" "$(get-value-from-user "$parameter")"
+  done
+}
+
+function check-secret {
+  local secret=$1
+  check-secret-set "$secret" || write-secret-value "$secret" "$(get-value-from-user "$secret" secret)"
+}
+
+function check-secrets {
+  local secret
+  for secret in "${SECRETS[@]}"; do check-secret "$secret"; done
 }
 
 function check-cognito-external-id {
@@ -35,17 +74,29 @@ function check-deletion-protection {
 
 function print-parameters {
   local parameter
-  echo "--- Deployment parameters in $(../aws.sh get-current-account-name) ---"
+  echo "--- Deployment parameters ---"
   for parameter in "${!PARAMETERS[@]}"; do
     echo "${PARAMETERS[$parameter]}: $(get-parameter-value "$parameter")"
   done
 }
 
-function check-deployment-parameters {
-  ../aws.sh check-current-account > /dev/null
-  check-cognito-external-id
-  check-deletion-protection
-  print-parameters
+function print-secrets {
+  local secret
+  echo "--- Secrets ---"
+  for secret in "${SECRETS[@]}"; do
+    check-secret-set "$secret" && echo "$secret"
+  done
 }
 
-check-deployment-parameters
+function check-deployment-parameters {
+  ../aws.sh check-current-account
+  check-cognito-external-id
+  check-deletion-protection
+  check-manual-parameters
+  check-secrets
+  print-parameters
+  print-secrets
+}
+
+[[ "$*" ]] || check-deployment-parameters
+[[ "$*" ]] && "$@"
