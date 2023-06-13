b7123851bd2cd1f99bca95aa6a1d0605a2c838ec
Karol C. Gancarz <karol.sinx@gmail.com>
2023-06-13 11:51:37 +0100

    SSE-2685: Consolidate config stacks
    Merge the domains and email identity stack into one
    Modify the script to get the subdomain name servers as needed
    Use one script to deploy the config stacks

---
+++ infrastructure/config/apply-account-config.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Apply infrastructure config to an AWS account
+cd "$(dirname "${BASH_SOURCE[0]}")"
+set -eu -o pipefail
+
+function deploy-config-stack {
+  local type=$1
+
+  ../deploy-sam-stack.sh "${@:2}" \
+    --validate \
+    --stack-name "$type"-config \
+    --template "$type".template.yml \
+    --tags sse:stack-type=config
+}
+
+function control-tower {
+  deploy-config-stack control-tower --tags sse:stack-role=account-management "$@"
+}
+
+function domain {
+  local account servers params
+
+  if [[ $(../aws.sh get-current-account-name) == production ]]; then
+    echo "Getting subdomain name servers..."
+    for account in development build staging integration; do
+      servers=$(gds aws di-onboarding-$account -- ../aws.sh get-stack-outputs domain-config HostedZoneNameServers) || continue
+      params+=("${account@u}NameServers=$(jq --raw-output ".value" <<< "$servers")")
+    done
+  fi
+
+  deploy-config-stack domain --tags sse:stack-role=dns ${params:+--parameters ${params[@]}} "$@"
+}
+
+"$@"
--- infrastructure/control-tower/apply-control-tower-config.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE[0]}")"
-set -eu
-
-../deploy-sam-stack.sh "$@" \
-  --validate \
-  --stack-name control-tower \
-  --template control-tower.template.yml \
-  --tags sse:stack-type=config sse:stack-role=account-management
