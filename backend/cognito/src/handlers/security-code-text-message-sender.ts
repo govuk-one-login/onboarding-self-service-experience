ced33c6f78aef120ea5f8ae40ad44153d37ba9eb
Rafal Surowiec <rafal.surowiec@digital.cabinet-office.gov.uk>
2023-06-22 13:19:50 +0100

    SSE-2666 Set up a KMS key to decrypt verification codes from Cognito

--- backend/cognito/src/handlers/security-code-text-message-sender.ts
+++ backend/cognito/src/handlers/security-code-text-message-sender.ts
@@ -1,15 +1,31 @@
 import {CustomSMSSenderTriggerEvent} from "aws-lambda";
 import {NotifyClient} from "notifications-node-client";
+import {toByteArray} from "base64-js";
+import {buildClient, CommitmentPolicy, KmsKeyringNode} from "@aws-crypto/client-node";
+
+const {decrypt} = buildClient(CommitmentPolicy.REQUIRE_ENCRYPT_ALLOW_DECRYPT);
+const generatorKeyId = process.env.KEY_ARN;
+const keyring = new KmsKeyringNode({generatorKeyId});
+
 const apiKey = process.env.NOTIFY_API_KEY;
 const templateId = process.env.SECURITY_CODE_TEXT_MESSAGE_TEMPLATE;
-
 const notifyClient = new NotifyClient(apiKey);
 
 export const lambdaHandler = async (event: CustomSMSSenderTriggerEvent): Promise<void> => {
-    const plainTextCode = event.request.code;
     const phoneNumber = event.request.userAttributes.phone_number;
+
+    if (!event.request.code) {
+        throw new Error("Missing code parameter");
+    }
+
+    const plainTextCode = await decryptText(event.request.code);
     await notifyClient.sendSms(templateId, phoneNumber, {
         personalisation: {code: plainTextCode},
         reference: null
     });
 };
+
+async function decryptText(encryptedText: string): Promise<string> {
+    const {plaintext} = await decrypt(keyring, toByteArray(encryptedText));
+    return plaintext.toString();
+}
--- backend/cognito/tests/handlers/security-code-text-message-sender.test.ts
+++ backend/cognito/tests/handlers/security-code-text-message-sender.test.ts
@@ -1,3 +1,5 @@
+import {toByteArray} from "base64-js";
+
 const sendSmsMock = jest.fn();
 
 const notifyClientMock = jest.fn().mockImplementation(() => {
@@ -8,7 +10,25 @@ const notifyClientMock = jest.fn().mockImplementation(() => {
 jest.mock("notifications-node-client", () => {
     return {NotifyClient: notifyClientMock};
 });
+
+const decryptMock = jest.fn().mockImplementation(() => {
+    return {
+        plaintext: "531836"
+    };
+});
+
+const buildClientMock = jest.fn().mockImplementation(() => {
+    return {
+        decrypt: decryptMock
+    };
+});
+
+jest.mock("@aws-crypto/client-node", () => {
+    return {buildClient: buildClientMock, CommitmentPolicy: jest.fn(), KmsKeyringNode: jest.fn()};
+});
+
 process.env.SECURITY_CODE_TEXT_MESSAGE_TEMPLATE = "ac38449b-efba-4e05-911a-83648caa8de5";
+process.env.KEY_ALIAS = "key-alias-01";
 
 import {lambdaHandler} from "../../src/handlers/security-code-text-message-sender";
 
@@ -18,7 +38,7 @@ describe("Custom SMS sender", () => {
             triggerSource: "CustomSMSSender_VerifyUserAttribute",
             request: {
                 type: "",
-                code: "531836",
+                code: "gtrsdwjstae4",
                 userAttributes: {
                     phone_number: "084168511"
                 }
@@ -29,13 +49,37 @@ describe("Custom SMS sender", () => {
             userName: "",
             callerContext: {awsSdkVersion: "", clientId: ""},
             response: ""
+        }).then(() => {
+            expect(sendSmsMock).toBeCalledTimes(1);
+            expect(sendSmsMock.mock.calls[0][0]).toBe("ac38449b-efba-4e05-911a-83648caa8de5");
+            expect(sendSmsMock.mock.calls[0][1]).toBe("084168511");
+            expect(sendSmsMock.mock.calls[0][2]).toEqual({
+                personalisation: {code: "531836"},
+                reference: null
+            });
+            expect(decryptMock).toBeCalledTimes(1);
+            expect(decryptMock.mock.calls[0][1]).toEqual(toByteArray("gtrsdwjstae4"));
         });
-        expect(sendSmsMock).toBeCalledTimes(1);
-        expect(sendSmsMock.mock.calls[0][0]).toBe("ac38449b-efba-4e05-911a-83648caa8de5");
-        expect(sendSmsMock.mock.calls[0][1]).toBe("084168511");
-        expect(sendSmsMock.mock.calls[0][2]).toEqual({
-            personalisation: {code: "531836"},
-            reference: null
+    });
+
+    it("should throw an error for missing code", () => {
+        lambdaHandler({
+            triggerSource: "CustomSMSSender_VerifyUserAttribute",
+            request: {
+                type: "",
+                code: null,
+                userAttributes: {
+                    phone_number: "084168511"
+                }
+            },
+            version: "1",
+            region: "",
+            userPoolId: "id-1",
+            userName: "",
+            callerContext: {awsSdkVersion: "", clientId: ""},
+            response: ""
+        }).catch((reason: Error) => {
+            expect(reason.message).toBe("Missing code parameter");
         });
     });
 });
