50c26bcad3cff57b368d5ab39950f6829031bfa7
Karol C. Gancarz <karol.sinx@gmail.com>
2023-07-28 19:18:48 +0100

    SSE-2769: Add TLS certificates
    Add certs to use when setting up an HTTPS endpoint for the frontend load balancer
    Export the admin domain values

--- infrastructure/config/apply-account-config.sh
+++ infrastructure/config/apply-account-config.sh
@@ -23,6 +23,7 @@ function network {
 
 function domain {
   local account servers params
+  account=$(../aws.sh get-current-account-name)
 
   if [[ $(../aws.sh get-current-account-name) == production ]]; then
     echo "Getting subdomain name servers..."
@@ -30,6 +31,8 @@ function domain {
       servers=$(gds aws di-onboarding-$account -- ../aws.sh get-stack-outputs domain-config HostedZoneNameServers) || continue
       params+=("${account@u}NameServers=$(jq --raw-output ".value" <<< "$servers")")
     done
+  else
+    params=(Subdomain="$account.")
   fi
 
   deploy-config-stack domain --tags sse:stack-role=dns ${params:+--parameters ${params[@]}} "$@"
