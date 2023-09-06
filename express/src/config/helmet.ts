59cdb42c2b42a981c5a6640fdf863812d81f377c
Martin Hillson <martin.hillson1@digital.cabinet-office.gov.uk>
2023-09-06 17:57:09 +0100

    Bring helmet config in line with product page.

--- express/src/config/helmet.ts
+++ express/src/config/helmet.ts
@@ -7,12 +7,31 @@ export default function Helmet() {
         contentSecurityPolicy: {
             directives: {
                 defaultSrc: ["'self'"],
-                scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
-                connectSrc: ["*"],
+                scriptSrc: [
+                    "'self'",
+                    "'unsafe-inline'",
+                    "https://www.googletagmanager.com",
+                    "https://www.google-analytics.com",
+                    "https://ssl.google-analytics.com"
+                ],
                 styleSrc: ["'self'", "'unsafe-inline'"],
-                imgSrc: ["'self'"],
+                imgSrc: ["'self'", "data:", "https://www.googletagmanager.com", "https://www.google-analytics.com"],
+                objectSrc: ["'none'"],
+                connectSrc: ["'self'", "https://www.google-analytics.com"],
                 formAction: ["'self'"]
             }
-        }
+        },
+        dnsPrefetchControl: {
+            allow: false
+        },
+        frameguard: {
+            action: "deny"
+        },
+        hsts: {
+            maxAge: 31536000, // 1 Year
+            preload: true,
+            includeSubDomains: true
+        },
+        permittedCrossDomainPolicies: false
     });
 }
