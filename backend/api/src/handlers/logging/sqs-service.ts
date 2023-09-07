a96ae0679e11787f9a527908b48b49fda5165ac2
james.carter <james.carter@digital.cabinet-office.gov.uk>
2023-09-07 09:58:43 +0100

    Fix Linting

--- backend/api/src/handlers/logging/sqs-service.ts
+++ backend/api/src/handlers/logging/sqs-service.ts
@@ -5,39 +5,41 @@ import * as process from "process";
 const client = new SQSClient({region: "eu-west-2"});
 
 export const sendSQSMessageToTxMAHandler = async (event: APIGatewayProxyEvent): Promise<any> => {
-    let payload: any;
+    let payload: string;
+    let response = {
+        statusCode: 500,
+        body: JSON.stringify("Error")
+    };
 
     if (event.body != null) {
         payload = JSON.parse(event.body);
-    }
-    let response;
-    const queueUrl = process.env.QUEUEURL;
-    console.log(queueUrl);
-    const messageParams = {
-        MessageBody: JSON.stringify(payload),
-        QueueUrl: queueUrl,
-    };
+        const queueUrl = process.env.QUEUEURL;
+        console.log(queueUrl);
+        const messageParams = {
+            MessageBody: JSON.stringify(payload),
+            QueueUrl: queueUrl
+        };
 
-    try {
-        const data = await client.send(new SendMessageCommand(messageParams));
-        if (data) {
-            console.log(JSON.stringify(payload));
-            const bodyMessage = 'Message Send to SQS - Here is MessageId: ' +data.MessageId;
-            response = {
-                statusCode: 200,
-                body: JSON.stringify(bodyMessage),
-            };
-        }else{
-            response = {
-                statusCode: 500,
-                body: JSON.stringify('Error')
-            };
+        try {
+            const data = await client.send(new SendMessageCommand(messageParams));
+            if (data) {
+                console.log(JSON.stringify(payload));
+                const bodyMessage = "Message Send to SQS - Here is MessageId: " + data.MessageId;
+                response = {
+                    statusCode: 200,
+                    body: JSON.stringify(bodyMessage)
+                };
+            } else {
+                response = {
+                    statusCode: 500,
+                    body: JSON.stringify("Error")
+                };
+            }
+            return response;
+        } catch (err) {
+            console.log("Error", err);
         }
-        return response;
-    }
-    catch (err) {
-        console.log("Error", err);
     }
 
     return response;
-}
+};
