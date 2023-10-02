8bda5bdb04eaa6dd9319cf9cdb68526fd8bbf512
melvabout <andrew.melvin@digital.cabinet-office.gov.uk>
2023-10-02 09:38:13 +0100

    Put comment on to one line

--- infrastructure/dev/deploy.sh
+++ infrastructure/dev/deploy.sh
@@ -124,8 +124,7 @@ function build-frontend-image {
   echo "» Building frontend"
   npm run build-express --include-workspace-root &> /dev/null
 
-  # You need to provide a DYNATRACE_PAAS_TOKEN environment variable which is your 
-  # dynatrace paas token.
+  # You need to provide a DYNATRACE_PAAS_TOKEN environment variable which is your dynatrace paas token.
   docker login khw46367.live.dynatrace.com -u khw46367 -p "$DYNATRACE_PAAS_TOKEN"
 
   echo "» Building Docker image"
