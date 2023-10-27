2e66666e77b5e400ec4909dcaef95df79fef4ee9
james.carter <james.carter@digital.cabinet-office.gov.uk>
2023-10-27 11:42:52 +0100

    Refactor Event Logging

--- backend/api/src/handlers/logging/txma-event.ts
+++ backend/api/src/handlers/logging/txma-event.ts
@@ -3,20 +3,20 @@ export interface TxMAEvent {
     event_name: string;
     component_id: string;
     session_id: string;
-    user?: TxMAUser;
+    user: TxMAUser;
     extensions?: TxMAExtension;
 }
 
 export interface TxMAUser {
-    ip_address?: string;
+    ip_address: string;
     user_id?: string;
     email?: string;
     phone?: string;
 }
 
 export interface TxMAExtension {
-    session_id?: string;
-    session_name?: string;
+    service_id?: string;
+    service_name?: string;
     outcome?: string;
     credential_type?: string;
 }
