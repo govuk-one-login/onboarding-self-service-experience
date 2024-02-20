3537946b5b0cbdc9c460ce762563422e849cb605
David Stump <david.stump@digital.cabinet-office.gov.uk>
2024-02-20 11:58:08 +0000

    SSE-3183:Added Flag for Cognito DR Processes to be disabled by default

--- express/src/middleware/process-sign-in-form.ts
+++ express/src/middleware/process-sign-in-form.ts
@@ -3,6 +3,7 @@ import {RequestHandler} from "express";
 import SelfServiceServicesService from "../services/self-service-services-service";
 import console from "console";
 import {convertToCountryPrefixFormat} from "../lib/mobile-number";
+import * as process from "process";
 
 export default function processSignInForm(template: string): RequestHandler {
     console.info("In processSignInForm()");
@@ -58,18 +59,20 @@ export default function processSignInForm(template: string): RequestHandler {
             if (error instanceof UserNotFoundException) {
                 console.log("UserNotFoundException");
 
-                const dynamoDBEntryResponse = await s4.getDynamoDBEntries(email);
-                const dynamoDBEntry = JSON.stringify(dynamoDBEntryResponse.data);
+                if (process.env.USE_COGNITO_DR == "true") {
+                    const dynamoDBEntryResponse = await s4.getDynamoDBEntries(email);
+                    const dynamoDBEntry = JSON.stringify(dynamoDBEntryResponse.data);
 
-                if (dynamoDBEntry != undefined && dynamoDBEntry.length > 2) {
-                    const clientDetails = JSON.parse(dynamoDBEntry);
-                    const userID = clientDetails.pk.S.substring(5); // Skip over 'user#' prefix
-                    const mobileNumber = convertToCountryPrefixFormat(clientDetails.phone.S);
+                    if (dynamoDBEntry != undefined && dynamoDBEntry.length > 2) {
+                        const clientDetails = JSON.parse(dynamoDBEntry);
+                        const userID = clientDetails.pk.S.substring(5); // Skip over 'user#' prefix
+                        const mobileNumber = convertToCountryPrefixFormat(clientDetails.phone.S);
 
-                    await s4.recoverCognitoAccount(req, email, password, mobileNumber);
+                        await s4.recoverCognitoAccount(req, email, password, mobileNumber);
 
-                    req.session.cognitoID = userID;
-                    return res.redirect("/sign-in/enter-text-code-then-continue-recovery");
+                        req.session.cognitoID = userID;
+                        return res.redirect("/sign-in/enter-text-code-then-continue-recovery");
+                    }
                 }
 
                 req.session.emailAddress = email;
