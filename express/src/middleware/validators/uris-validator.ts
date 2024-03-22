0ed7ba56c744d8df8411d8f9fabdc9ed54606fe3
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-03-22 12:07:34 +0000

    Adapts uri validation function to be more flexible. This adds two things, one the optional URI limit mentioned in the previous commit. This also adds a flexible URI Name to the function so we can specify URIs other than redirectUris

--- express/src/middleware/validators/uris-validator.ts
+++ express/src/middleware/validators/uris-validator.ts
@@ -1,12 +1,12 @@
 import {RequestHandler} from "express";
 import validate from "../../lib/validators/uris-validator";
 
-export default function validateUris(template: string): RequestHandler {
+export default function validateUris(template: string, uriName = "redirectUris", uriLimit?: number): RequestHandler {
     console.info("In validateUris()");
 
     return (req, res, next) => {
-        const uris: string = req.body.redirectUris;
-        const result = validate(uris.split(" ").filter(url => url.length > 0));
+        const uris: string = req.body[uriName];
+        const result = validate(uris.split(" ").filter(url => url.length > 0), uriLimit);
 
         if (result.isValid) {
             return next();
@@ -20,7 +20,7 @@ export default function validateUris(template: string): RequestHandler {
                 redirectUris: req.body.redirectUris
             },
             errorMessages: {
-                redirectUris: result.errorMessage
+                [uriName]: result.errorMessage
             }
         });
     };
