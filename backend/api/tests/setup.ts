99ae67e9d80f5c5facf55e368a8fbd793a85b52f
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-03-26 09:46:56 +0000

    [BAU]: Adds Env Var for State Machine ARN
    Our lambdas have the state machine ARN configured as an env var, so this adds a mock ARN in the jest setup to mock this behaviour out

--- backend/api/tests/setup.ts
+++ backend/api/tests/setup.ts
@@ -1,2 +1,4 @@
 export const TEST_DYNAMO_TABLE_NAME = "TEST_TABLE_NAME";
+export const TEST_STATE_MACHINE_ARN = "arn:aws:eu-west-2:stateMachine:123456";
 process.env.TABLE = TEST_DYNAMO_TABLE_NAME;
+process.env.STATE_MACHINE_ARN = TEST_STATE_MACHINE_ARN;
