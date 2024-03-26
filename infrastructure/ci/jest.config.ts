a1dcc49b2b0d8e5b2abe40305f61b2bf3d361b2c
abdul-gds <abdul.aziz@digital.cabinet-office.gov.uk>
2024-03-26 11:58:11 +0000

    SSE-3267 delete pipeline, ghd role, deploy params moved to infra repo

--- infrastructure/ci/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import {Config} from "jest";
-import baseConfig from "../../jest.config";
-
-export default {
-    ...baseConfig,
-    reporters: [["github-actions", {silent: false}], "summary"]
-} satisfies Config;
