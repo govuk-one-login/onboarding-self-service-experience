30d5a11150fbb791986ed227e496209f6b2efd54
Raghu Kora <raghava.kora@digital.cabinet-office.gov.uk>
2024-03-28 13:09:51 +0000

    added screenshot to the failed scenario

--- ui-automation-tests/support/test-setup.ts
+++ ui-automation-tests/support/test-setup.ts
@@ -1,11 +1,11 @@
 import {After, AfterAll, Before, BeforeAll, setWorldConstructor} from "@cucumber/cucumber";
 import {IWorldOptions} from "@cucumber/cucumber/lib/support_code_library_builder/world";
 import puppeteer, {Browser, Page} from "puppeteer";
-
+import fse from "fs-extra";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const {World} = require("@cucumber/cucumber");
 
-let browser: Browser;
+let browser: Browser, counter: number;
 
 export class TestContext extends World {
     private browserPage: Page | undefined;
@@ -32,6 +32,15 @@ export class TestContext extends World {
 }
 
 BeforeAll(async function () {
+    counter = 0;
+    if (fse.pathExistsSync("output/screenshots")) {
+        console.log("Clear screenshots directory ...");
+        fse.removeSync("output/screenshots/");
+        // recreate directory
+        fse.ensureDirSync("output/screenshots");
+    } else {
+        fse.ensureDirSync("output/screenshots");
+    }
     console.log(`Running tests against ${process.env.HOST ?? "local"}`);
     browser = await puppeteer.launch({headless: !process.env.SHOW_BROWSER});
 });
@@ -41,9 +50,18 @@ Before(async function (this: TestContext) {
     this.page = await browser.newPage();
 });
 
-After(async function (this: TestContext) {
-    if (!process.env.SHOW_BROWSER) {
-        await this.page.close();
+After(async function (this: TestContext, scenario) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const result = scenario.result.status;
+    const name = scenario.pickle.name.replace(/ /g, "-");
+    if (result === "FAILED") {
+        counter++;
+        const stream = await this.page.screenshot({
+            path: `./output/screenshots/${counter}-${result}-[${name}].jpeg`,
+            fullPage: true
+        });
+        return this.attach(stream, "image/jpeg");
     }
 });
 
