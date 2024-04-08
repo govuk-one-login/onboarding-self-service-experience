70eacf31920a20cb5311ce5713bbd7052fe26b6e
Martin Hillson <martin.hillson1@digital.cabinet-office.gov.uk>
2024-04-08 15:31:56 +0100

    SSE-3309 - sync service name changes in Client Registry

--- backend/api/src/handlers/dynamodb/update-service.ts
+++ backend/api/src/handlers/dynamodb/update-service.ts
@@ -1,14 +1,25 @@
-import {APIGatewayProxyEvent} from "aws-lambda";
-import DynamoDbClient from "../../dynamodb-client";
+import DynamoDbClient, {Updates} from "../../dynamodb-client";
 
 const client = new DynamoDbClient();
 
-export const updateServiceHandler = async (event: APIGatewayProxyEvent): Promise<{statusCode: number; body: string}> => {
-    const body = JSON.parse(event.body as string);
+export type updateServicePayload = {
+    serviceId: string;
+    selfServiceClientId: string;
+    clientId: string;
+    updates: Updates;
+};
+
+export type updateServiceHandlerInvokeEvent = {
+    statusCode: number;
+    body: string;
+};
+
+export const updateServiceHandler = async (event: updateServiceHandlerInvokeEvent): Promise<{statusCode: number; body: string}> => {
+    const payload: updateServicePayload = JSON.parse(event.body);
     const response = {statusCode: 200, body: JSON.stringify("OK")};
 
     await client
-        .updateService(body.serviceId, body.updates)
+        .updateService(payload.serviceId, payload.updates)
         .then(updateItemCommandOutput => {
             response.statusCode = 200;
             response.body = JSON.stringify(updateItemCommandOutput);
