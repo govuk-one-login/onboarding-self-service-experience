4aeaba53d9b7f3fbf4f1e1d4374470d24e0a20b4
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-04-26 16:43:31 +0100

    BAU: Adds a mock CustomMessageForgotPasswordTriggerEvent
    To test the lambda we need to create a mock invoke event, so I have done that here with some other test constants also exported to be resused in the tests

--- backend/cognito/tests/mocks.ts
+++ backend/cognito/tests/mocks.ts
@@ -1,4 +1,34 @@
-import {CustomSMSSenderTriggerEvent} from "aws-lambda";
+import {CustomMessageForgotPasswordTriggerEvent, CustomSMSSenderTriggerEvent} from "aws-lambda";
 
 export const smsSenderTrigger = (number: string, code?: string) =>
     ({request: {code: code, userAttributes: {phone_number: number}}} as unknown as CustomSMSSenderTriggerEvent);
+
+export const TEST_PROTOCOL = "https";
+export const TEST_HOST = "some.service.gov.uk";
+export const TEST_CODE_PARAM = "12345";
+export const TEST_EMAIL = "someEmail";
+export const mockCustomPasswordResetMessage: CustomMessageForgotPasswordTriggerEvent = {
+    request: {
+        userAttributes: {},
+        codeParameter: TEST_CODE_PARAM,
+        linkParameter: "someLinkParam",
+        usernameParameter: null,
+        clientMetadata: {
+            protocol: TEST_PROTOCOL,
+            host: TEST_HOST,
+            use_recovered_account_url: "null",
+            username: TEST_EMAIL
+        }
+    },
+    response: {
+        emailMessage: null,
+        emailSubject: null,
+        smsMessage: null
+    },
+    version: "",
+    region: "",
+    userPoolId: "",
+    triggerSource: "CustomMessage_ForgotPassword",
+    userName: "",
+    callerContext: {awsSdkVersion: "1.0", clientId: "someClientID"}
+};
