f03c8a9a827aed053aa88de80c3ed0a374626941
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-05-07 12:07:33 +0100

    BAU: Use base config in root jest config
    We should use the base config in our root dir jest config to ensure things are consistent between running tests. This now enables accurate coverage collection as we're using the paths set out in the base config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,7 @@
 import {Config} from "jest";
+import baseConfig from "./jest.config.base";
 
 export default {
+    ...baseConfig,
     projects: ["express/jest.config.ts", "backend/*/jest.config.ts"]
 } satisfies Config;
