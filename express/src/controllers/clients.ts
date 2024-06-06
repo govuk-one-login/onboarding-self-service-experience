8c87a2dae52cbf5e184de113d1377b65da58bf08
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-06-06 09:58:19 +0100

    SSE-3417: Adds changeIdVerificationEnabledUri link

--- express/src/controllers/clients.ts
+++ express/src/controllers/clients.ts
@@ -81,7 +81,8 @@ export const showClient: RequestHandler = async (req, res) => {
                     : `/services/${serviceId}/clients/${authClientId}/${selfServiceClientId}/change-public-key?publicKey=${encodeURIComponent(
                           displayedKey
                       )}`,
-            changeContacts: `/services/${serviceId}/clients/${authClientId}/${selfServiceClientId}/enter-contact`
+            changeContacts: `/services/${serviceId}/clients/${authClientId}/${selfServiceClientId}/enter-contact`,
+            changeIdVerificationEnabledUri: `/services/${serviceId}/clients/${authClientId}/${selfServiceClientId}/enter-identity-verification`
         }
     });
 
