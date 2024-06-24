498f9052d4d8c04b5135915f30adb4e738ccbd32
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-06-24 13:33:30 +0100

    BAU: Updates the error handling in update lambda
    We were previously catching errors and returning a status code and error
    message. This meant we passed through client registry errors which made
    it harder to debug issues. I have re-implemented the throwing with some
    additonal logging as we do not want to pass these error states through
    to the next part of the step function

--- backend/api/src/handlers/auth/update-client.ts
+++ backend/api/src/handlers/auth/update-client.ts
@@ -1,5 +1,5 @@
 import {APIGatewayProxyResult} from "aws-lambda";
-import axios from "axios";
+import axios, {AxiosResponse, isAxiosError} from "axios";
 
 export type UpdateClientPayload = {
     clientId: string;
@@ -11,41 +11,49 @@ export type UpdateClientPayload = {
 // Handler is invoked by step function not API Gateway
 //The event type is not APIGatewayProxyEvent but the custom JSON payload from the step function invokes
 export const updateClientInRegistryHandler = async (event: UpdateClientPayload): Promise<APIGatewayProxyResult> => {
+    console.log("Update client request received")
     const url = process.env.AUTH_REGISTRATION_BASE_URL + "/connect/register/" + event.clientId;
-    let response: never[] = [];
-    let statusCode = 200;
 
     if (event.updates.hasOwnProperty("service_name")) {
         // Service Name in SSE maps to Client Name in Registry
         event.updates.client_name = event.updates.service_name;
     }
 
-    await axios
+    const response = await axios
         .put(url, event.updates, {
             headers: {
                 "Content-Type": "application/json",
                 "X-API-Key": process.env.AUTH_API_KEY as string
             }
         })
-        .then(result => {
-            response = result.data;
-            if (event.updates.hasOwnProperty("client_name")) {
-                // Must remove this if we added it otherwise later functions will break
-                delete event.updates["client_name"];
-            }
-        })
+        .then(handleResponse(event))
         .catch(error => {
-            statusCode = error.response.statusCode;
-            response = error.response.data.error;
+            if (isAxiosError(error)) {
+                console.error(
+                    `Client registry request failed with response: "${error.response?.status}" and message "${error.response?.data}"`
+                );
+            }
+            throw error;
         });
 
     return {
-        statusCode: statusCode,
+        statusCode: response.status,
         body: JSON.stringify({
-            ...response,
-            updates: event.updates,
-            serviceId: event.serviceId,
-            selfServiceClientId: event.selfServiceClientId
+            ...response.response,
+            updates: response.event.updates,
+            serviceId: response.event.serviceId,
+            selfServiceClientId: response.event.selfServiceClientId
         })
+    };
+};
+
+const handleResponse = (event: UpdateClientPayload) => {
+    return (result: AxiosResponse) => {
+        if (event.updates.hasOwnProperty("client_name")) {
+            // Must remove this if we added it otherwise later functions will break
+            delete event.updates["client_name"];
+        }
+
+        return {response: result.data, status: result.status, event};
     };
 };
