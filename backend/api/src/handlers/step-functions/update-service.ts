57c9ea60f5afa08407c300c2a91d302879f5a473
Andrew Hughes-Onslow <andrew.hughes-onslow@digital.cabinet-office.gov.uk>
2024-08-07 17:27:01 +0100

    ATO-814: Added step function error handling and ecs client error handling.

--- backend/api/src/handlers/step-functions/update-service.ts
+++ backend/api/src/handlers/step-functions/update-service.ts
@@ -2,6 +2,5 @@ import {APIGatewayProxyEvent, APIGatewayProxyResult, Context} from "aws-lambda";
 import {stepFunctionHandler} from "./step-function-handler";
 
 export const doUpdateServiceHandler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.info("In doUpdateServiceHandler() callback from step function. Context (" + JSON.stringify(context) + ")");
-    return stepFunctionHandler(event);
+    return stepFunctionHandler(event, context);
 };
