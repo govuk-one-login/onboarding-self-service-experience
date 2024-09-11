5be9124b1e9b13e1dff039c819728f5eb044b25f
Ryan Andrews <ryan.andrews@digital.cabinet-office.gov.uk>
2024-09-11 16:55:07 +0100

    ATO-1022: Adds test constant

--- backend/api/tests/handlers/constants.ts
+++ backend/api/tests/handlers/constants.ts
@@ -21,6 +21,7 @@ export const TEST_USER_CONFIG: Record<string, AttributeValue> = {
 };
 export const TEST_COGNITO_USER_ID = "e20b814d-55b1-4e68-a28d-3a0c4818adfa";
 export const TEST_SERVICE_ID = "488c26e5-6147-4b8e-be05-031e00310fdb";
+export const TEST_SELF_SERVICE_CLIENT_ID = "128c26e5-6147-4b4e-be15-031e00310gyj";
 export const TEST_DEFAULT_PUBLIC_KEY =
     "MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEAp2mLkQGo24Kz1rut0oZlviMkGomlQCH+iT1pFvegZFXq39NPjRWyatmXp/XIUPqCq9Kk8/+tq4Sgjw+EM5tATJ06j5r+35of58ATGVPniW//IhGizrv6/ebGcGEUJ0Y/ZmlCHYPV+lbewpttQ/IYKM1nr3k/Rl6qepbVYe+MpGubluQvdhgUYel9OzxiOvUk7XI0axPquiXzoEgmNNOai8+WhYTkBqE3/OucAv+XwXdnx4XHmKzMwTv93dYMpUmvTxWcSeEJ/4/SrbiK4PyHWVKU2BozfSUejVNhahAzZeyyDwhYJmhBaZi/3eOOlqGXj9UdkOXbl3vcwBH8wD30O9/4F5ERLKxzOaMnKZ+RpnygWF0qFhf+UeFMy+O06sdgiaFnXaSCsIy/SohspkKiLjNnhvrDNmPLMQbQKQlJdcp6zUzI7Gzys7luEmOxyMpA32lDBQcjL7KNwM15s4ytfrJ46XEPZUXESce2gj6NazcPPsrTa/Q2+oLS9GWupGh7AgMBAAE=";
 
