131a692bb2c551e829334380de33e9c6d342d8d3
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2025-09-22 17:00:39 +0100

    ATO-1837: fix authoriser test

--- express/tests/middleware/authoriser.test.ts
+++ express/tests/middleware/authoriser.test.ts
@@ -1,19 +1,21 @@
 import checkAuthorisation from "../../src/middleware/authoriser";
 import {request, response} from "../mocks";
 
-const next = jest.fn();
-const req = request();
-const res = response();
+const mockNext = jest.fn();
+const mockRes = response();
 
 describe("Verify access token in the session", () => {
-    it("Redirect to the session timeout page if there is no access token", async () => {
-        await checkAuthorisation(req, res, next);
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
 
-        // For 'resume-before-password' and 'resume-after-password' Authorisation is re-instigated following re-entry of
-        // required authoristation codes on Pages (i.e. EMail Code or Password) and therefore checking can be bypassed here.
-        if (req.path !== "/resume-before-password" && req.path !== "/resume-after-password" && req.method === "POST") {
-            expect(next).not.toHaveBeenCalled();
-            expect(res.redirect).toHaveBeenCalledWith("/session-timeout");
-        }
+    it("Redirect to the session timeout page if there is no access token", async () => {
+        const mockReq = request({
+            path: "/test-path"
+        });
+        await checkAuthorisation(mockReq, mockRes, mockNext);
+
+        expect(mockNext).not.toHaveBeenCalled();
+        expect(mockRes.redirect).toHaveBeenCalledWith("/session-timeout");
     });
 });
